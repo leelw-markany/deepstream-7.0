fb3c21c6cf80a4bc745ffcc728c7119b204aaa3c
Laewook Lee <leelw@markany.com>
2024-09-06 13:36:13 +0900

    -

--- research/onboarding/20-1.run-deepstream-wsl.sh
+++ research/onboarding/20-1.run-deepstream-wsl.sh
@@ -10,30 +10,28 @@ export DISPLAY=$(grep -oP "(?<=nameserver ).+" /etc/resolv.conf):0
 xhost +
 sudo rm -rf /tmp/.X11-unix
 
-docker run -it \
-  --rm \
-  --gpus all \
-  --net=host \
-  --name=deepstream \
-  -v /tmp/.X11-unix/:/tmp/.X11-unix \
-  -e DISPLAY=$DISPLAY \
-  deepstream:7.0
-
-
 # docker run -it \
 #   --rm \
 #   --gpus all \
 #   --net=host \
 #   --name=deepstream \
 #   -v /tmp/.X11-unix/:/tmp/.X11-unix \
-#   -v ./deepstream-6.4/samples/models/Tracker/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/Tracker \
-#   -v ./deepstream-6.4/samples/models/peoplenet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/peoplenet \
-#   -v ./deepstream-6.4/samples/models/actionrecognitionnet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/actionrecognitionnet \
-#   -v ./deepstream-6.4/samples/models/bodypose3dnet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/bodypose3dnet \
-#   -v ./deepstream_reference_apps/:/opt/nvidia/deepstream/deepstream-6.4/sources/apps/deepstream_reference_apps \
 #   -e DISPLAY=$DISPLAY \
-#   -e CUDA_CACHE_DISABLE=0 \
 #   deepstream:7.0
+
+docker run -it \
+  --rm \
+  --gpus all \
+  --net=host \
+  --name=deepstream \
+  -v /tmp/.X11-unix/:/tmp/.X11-unix \
+  -v ./deepstream-7.0/samples/models/Tracker/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/Tracker \
+  -v ./deepstream-7.0/samples/models/peoplenet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/peoplenet \
+  -v ./deepstream-7.0/samples/models/actionrecognitionnet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/actionrecognitionnet \
+  -v ./deepstream-7.0/samples/models/bodypose3dnet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/bodypose3dnet \
+  -v ./deepstream_reference_apps/:/opt/nvidia/deepstream/deepstream-7.0/sources/apps/deepstream_reference_apps \
+  -e DISPLAY=$DISPLAY \
+  deepstream:7.0
 
 exit $?
 
