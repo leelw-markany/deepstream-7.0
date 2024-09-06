54ca5f2c47f89bc01290e05e5febba63c46896be
Laewook Lee <leelw@markany.com>
2024-09-06 14:15:06 +0900

    -

--- research/onboarding/20-1.run-deepstream-wsl.sh
+++ research/onboarding/20-1.run-deepstream-wsl.sh
@@ -25,12 +25,20 @@ docker run -it \
   --net=host \
   --name=deepstream \
   -v /tmp/.X11-unix/:/tmp/.X11-unix \
-  -v ./deepstream-7.0/samples/models/Tracker/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/Tracker \
-  -v ./deepstream-7.0/samples/models/peoplenet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/peoplenet \
-  -v ./deepstream-7.0/samples/models/actionrecognitionnet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/actionrecognitionnet \
-  -v ./deepstream-7.0/samples/models/bodypose3dnet/:/opt/nvidia/deepstream/deepstream-6.4/samples/models/bodypose3dnet \
+  -v ./deepstream-7.0/samples/models/Tracker/:/opt/nvidia/deepstream/deepstream-7.0/samples/models/Tracker \
+  -v ./deepstream-7.0/samples/models/peoplenet/:/opt/nvidia/deepstream/deepstream-7.0/samples/models/peoplenet \
+  -v ./deepstream-7.0/samples/models/actionrecognitionnet/:/opt/nvidia/deepstream/deepstream-7.0/samples/models/actionrecognitionnet \
+  -v ./deepstream-7.0/samples/models/bodypose3dnet/:/opt/nvidia/deepstream/deepstream-7.0/samples/models/bodypose3dnet \
+  -v ./deepstream-7.0/samples/configs/deepstream-app/:/opt/nvidia/deepstream/deepstream-7.0/samples/configs/deepstream-app \
+  -v ./deepstream-7.0/sources/includes/:/opt/nvidia/deepstream/deepstream-7.0/sources/includes \
+  -v ./deepstream-7.0/sources/apps/:/opt/nvidia/deepstream/deepstream-7.0/sources/apps \
+  -v ./deepstream-7.0/sources/objectDetector_Yolo/:/opt/nvidia/deepstream/deepstream-7.0/sources/objectDetector_Yolo \
+  -v ./deepstream-7.0/sources/tracker_ReID/:/opt/nvidia/deepstream/deepstream-7.0/sources/tracker_ReID \
   -v ./deepstream_reference_apps/:/opt/nvidia/deepstream/deepstream-7.0/sources/apps/deepstream_reference_apps \
   -e DISPLAY=$DISPLAY \
+  -e CUDA_CACHE_DISABLE=0 \
+  -p 8554:8554 \
+  -p 5400:5400 \
   deepstream:7.0
 
 exit $?
