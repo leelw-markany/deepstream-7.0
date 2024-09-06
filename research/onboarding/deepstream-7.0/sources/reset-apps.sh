a68889dbbe1729cb14cc0034131875c38989d556
Laewook Lee <leelw@markany.com>
2024-09-06 13:52:33 +0900

    -

---
+++ research/onboarding/deepstream-7.0/sources/reset-apps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT=`realpath -s $0`
+SCRIPTPATH=`dirname ${SCRIPT}`
+SCRIPTNAME=`basename ${SCRIPT}`
+cd ${SCRIPTPATH}
+set -euo pipefail
+
+IMAGE=deepstream:7.0
+SOURCE=/opt/nvidia/deepstream/deepstream-7.0/sources/apps
+DESTINATION=./apps
+
+container=$(docker create $IMAGE)
+sudo rm -rf ./$DESTINATION
+docker cp $container:$SOURCE $DESTINATION
+docker rm $container
+
+exit $?
